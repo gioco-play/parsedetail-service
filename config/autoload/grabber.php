b2fd4fe00ab8af4196ab1434e874d5b0022fdeea
Ellie Su <ellie.su@goldenf.co>
2021-02-08 14:37:28 +0800

    first commit

---
+++ config/autoload/grabber.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+return [
+    'worker_num' => (int) env('WORKER_NUM', 20)
+];
