e44fa68569bc11f9909d21156987676ed2644d93
Ellie Su <ellie.su@goldenf.co>
2021-02-23 17:21:28 +0800

    modify mahjong

---
+++ app/Helper/Constant/MahjongCode.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Helper\Constant;
+
+class MahjongCode
+{
+    /**
+     * 花色
+     */
+    const WAN = 'W'; // 萬
+    const DOT = 'D'; // 筒
+    const BAMS = 'B'; // 條
+    const HONOUR = 'H'; // 字
+    const FLOWER = 'F'; // 花
+
+    /**
+     * 數字
+     */
+    const ONE = '1';
+    const TWO = '2';
+    const THREE = '3';
+    const FOUR = '4';
+    const FIVE = '5';
+    const SIX = '6';
+    const SEVEN = '7';
+    const EIGHT = '8';
+    const NINE = '9';
+
+    /**
+     * 字: 東南西北中發白
+     */
+    const EAST = '1';
+    const SOUTH = '2';
+    const WEST = '3';
+    const NORTH = '4';
+    const RED = '5';
+    const GREEN = '6';
+    const WHITE = '7';
+
+    /**
+     * 花: 梅蘭竹菊春夏秋冬
+     */
+    const PLUM = '1';
+    const ORCHID = '2';
+    const BAMBOO = '3';
+    const CHRYSANTHEMUM = '4';
+    const SPRING = '5';
+    const SUMMER = '6';
+    const AUTUMN = '7';
+    const WINTER = '8';
+}
