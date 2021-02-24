a04d69f3a71c0d9e7dbe4415cbc663de7a4c6f1b
Ellie Su <ellie.su@goldenf.co>
2021-02-24 15:05:15 +0800

    add dice

---
+++ app/Helper/Constant/DiceCode.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Helper\Constant;
+
+class DiceCode
+{
+    /**
+     * 點數
+     */
+    const ONE = '1';
+    const TWO = '2';
+    const THREE = '3';
+    const FOUR = '4';
+    const FIVE = '5';
+    const SIX = '6';
+}
