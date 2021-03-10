e69652c99b0774f02c3abeaad306549887f4e33f
Ellie Su <ellie.su@goldenf.co>
2021-03-10 17:20:55 +0800

    modify detail blade

--- storage/view/layouts/elements/round-no.blade.php
+++ storage/view/layouts/elements/round-no.blade.php
@@ -1,6 +1,8 @@
 <div class="result mt-3">
     <div class="bottom d-flex justify-content-center align-items-center">
+        @if (isset($game_result['round_no']))
         <span>{{ __('detail.round_no', [], $lang) }}：{{ $game_result['round_no'] }}</span>
+        @endif
 
         @include('layouts.elements.game-result')
 
