746d6d7cc60015e8dcf2d0353c51cd4796b26f43
Ellie Su <ellie.su@goldenf.co>
2021-03-11 16:23:43 +0800

    modify blade

--- storage/view/live/player-list.blade.php
+++ storage/view/live/player-list.blade.php
@@ -6,8 +6,8 @@
 
         @foreach ($game_result['player'] as $k => $p)
         <div class="d-flex align-items-center mb-2 row w-100">
-            <span class="result-word text-{{ ($k == 0) ? 'red' : 'blue'}} text-right col-5">{{ __($vendor_code . '.' . $p['name'], ['seat' => $p['seat']], $lang) }}</span>
-            <div class="d-flex col-7">
+            <span class="result-word text-{{ ($k == 0) ? 'red' : 'blue'}} text-right col-3">{{ __($vendor_code . '.' . $p['name'], ['seat' => $p['seat']], $lang) }}</span>
+            <div class="d-flex flex-wrap col">
                 @each ('layouts.elements.card', array_map(function ($card) use ($parse_type) {
                     return $parse_type . '-' . $card;
                 }, $p['card']), 'card')
